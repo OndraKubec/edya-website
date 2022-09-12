336bf00c4bf9558556f832a596f52152b6a47204
Ondra Kubec <ondra.kubec@icloud.com>
2022-09-12 22:45:29 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,265 @@
+<template>
+<div class="">
+  <landing-section></landing-section>
+
+  <!--  ABOUT -->
+  <div class="container lg:mt-24">
+    <div class="">
+      <h2>ABOUT US</h2>
+      <p>We are producing modern photos, videos, graphic designs, websites and other physical promotional materials under a single roof.</p>
+      <p>We are a team made up of young creatives, full of ideas, and industry professionals with years of experience combining new innovative ideas with experience of professionals resulting in a modern looking and functional product. </p>
+      <p>Lately, we’ve been broadcasting and co-producing small to mid-sized hybrid events, combining live and remote audiences in one event.</p>
+    </div>
+  </div>
+
+  <!-- VIDEO -->
+
+  <img src="@/assets/gradient-black.png" alt="" class="w-screen">
+
+  <div class="relative">
+    <div class="grad-reversed">
+      <div class="container stripes-reversed">
+        <div class="service-img">
+          <div class="service-video left-0 -translate-y-32 lg:-translate-y-48 scale-150"></div>
+        </div>
+        <div>
+          <h2>Video</h2>
+          <p>We take videos seriously. They are the photos of the new age. We create cinematic videos of all sorts raging from short commercials, hour long documentaries, over to producing a multi-camera broadcast.</p>
+          </div>
+      </div>
+    </div>
+    <div class="hidden lg:block absolute bottom-0 right-0 side-finger" style="transform: scaleX(-1) translateY(8rem);">
+        <svg height="auto" viewBox="0 0 472 353" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M-109.845 142.673L471.155 352.673L471.155 262.173L-109.845 54.989L-109.845 142.673Z" fill="url(#paint0_linear_210_11)"/>
+            <path d="M-151.401 104.249L377.155 292.809L377.155 194.294L-151.401 0.132221L-151.401 104.249Z" fill="url(#paint1_linear_210_11)"/>
+            <defs>
+            <linearGradient id="paint0_linear_210_11" x1="-113.543" y1="248.273" x2="429.95" y2="105.412" gradientUnits="userSpaceOnUse">
+            <stop offset="0.5" stop-color="#EEB13A"/>
+            <stop offset="1" stop-color="#EEBB3A" stop-opacity="0"/>
+            </linearGradient>
+            <linearGradient id="paint1_linear_210_11" x1="-113.543" y1="248.273" x2="429.95" y2="105.412" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#FFB800"/>
+            <stop offset="0.541667" stop-color="#FFB800"/>
+            <stop offset="1" stop-color="#DBA10B" stop-opacity="0.22"/>
+            </linearGradient>
+            </defs>
+        </svg>
+    </div>
+  </div>
+
+  <img src="@/assets/gradient-black2.png" alt="" class="w-screen">
+  
+  <!-- PHOTO -->
+  <div class="relative">
+    <div class="container stripes">
+      <div>
+        <h2>Photo</h2>
+        <p>Our team of photographers jump from project to project. They are skilled to work in every environment to capture the perfect photos of your products, events and even weddings.</p>
+      </div>
+      <div class="service-img">
+        <div class="service-photo  -translate-y-2 lg:-translate-y-12 scale-150"></div>
+      </div>
+    </div>
+  </div>
+  
+
+  <!-- DESIGN -->
+
+  <img src="@/assets/gradient-black.png" alt=""  style="transform: scaleX(-1)" class="w-screen">
+
+  <div class="relative">
+    <div class="grad">
+      <div class="container stripes-reversed">
+        <div class="service-img">
+
+        </div>
+        <div>
+          <h2>Graphic design</h2>
+          <p>It all starts here. Alfa and omega of our company. Our talented team of graphic designers fulfill all of our graphics needs, creating minimalistic logos, designing modern websites, visualizing physical content and much more.</p>
+          </div>
+      </div>
+    </div>
+    <div class="hidden lg:block absolute bottom-0 left-0 side-finger" style="transform: translateY(5rem); z-index:10;">
+        <svg height="auto" viewBox="0 0 472 353" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M-109.845 142.673L471.155 352.673L471.155 262.173L-109.845 54.989L-109.845 142.673Z" fill="url(#paint0_linear_210_11)"/>
+            <path d="M-151.401 104.249L377.155 292.809L377.155 194.294L-151.401 0.132221L-151.401 104.249Z" fill="url(#paint1_linear_210_11)"/>
+            <defs>
+            <linearGradient id="paint0_linear_210_11" x1="-113.543" y1="248.273" x2="429.95" y2="105.412" gradientUnits="userSpaceOnUse">
+            <stop offset="0.5" stop-color="#EEB13A"/>
+            <stop offset="1" stop-color="#EEBB3A" stop-opacity="0"/>
+            </linearGradient>
+            <linearGradient id="paint1_linear_210_11" x1="-113.543" y1="248.273" x2="429.95" y2="105.412" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#FFB800"/>
+            <stop offset="0.541667" stop-color="#FFB800"/>
+            <stop offset="1" stop-color="#DBA10B" stop-opacity="0.22"/>
+            </linearGradient>
+            </defs>
+        </svg>
+    </div>
+  </div>
+
+  <img src="@/assets/gradient-black2.png" alt=""  style="transform: scaleX(-1)" class="w-screen">
+
+  <!-- PRINT -->
+
+  <div class="relative">
+    <div class="">
+      <div class="container stripes">
+        <div>
+          <h2>Print</h2>
+          <p>Want a business card or a lorry-sized print? That’s not a problem for us, since creating such materials is our daily bread.</p>
+        </div>
+        <div class="service-img">
+
+        </div>
+      </div>
+    </div>
+    <div class="hidden lg:block absolute bottom-0 right-0 side-finger" style="transform: scaleX(-1) translateY(0rem);">
+        <svg height="auto" viewBox="0 0 472 353" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M-109.845 142.673L471.155 352.673L471.155 262.173L-109.845 54.989L-109.845 142.673Z" fill="url(#paint0_linear_210_11)"/>
+            <path d="M-151.401 104.249L377.155 292.809L377.155 194.294L-151.401 0.132221L-151.401 104.249Z" fill="url(#paint1_linear_210_11)"/>
+            <defs>
+            <linearGradient id="paint0_linear_210_11" x1="-113.543" y1="248.273" x2="429.95" y2="105.412" gradientUnits="userSpaceOnUse">
+            <stop offset="0.5" stop-color="#EEB13A"/>
+            <stop offset="1" stop-color="#EEBB3A" stop-opacity="0"/>
+            </linearGradient>
+            <linearGradient id="paint1_linear_210_11" x1="-113.543" y1="248.273" x2="429.95" y2="105.412" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#FFB800"/>
+            <stop offset="0.541667" stop-color="#FFB800"/>
+            <stop offset="1" stop-color="#DBA10B" stop-opacity="0.22"/>
+            </linearGradient>
+            </defs>
+        </svg>
+    </div>
+  </div>
+
+  <!-- WEB -->
+
+  <img src="@/assets/gradient-black.png" alt="" class="w-screen">
+
+  <div class="relative">
+    <div class="grad-reversed">
+      <div class="relative">
+        <div class="container stripes-reversed">
+          <div class="service-img">
+            <div class="service-code left-0 -translate-y-8 lg:-translate-y-24 scale-150"></div>
+          </div>
+          <div>
+            <h2>Websites</h2>
+            <p>Nowadays, all content is consumed on websites, therefore, it is critical to have a modern looking and functional website with quality photos, videos and graphics.</p>
+            
+          </div>
+        </div>
+      </div>
+
+  <!-- CONTACT -->
+
+      <div class="hidden md:block absolute drop-shadow right-0 bottom-0 w-screen bg-black pt-32" style="height: 85vh; clip-path: polygon(0 0vw, 30vw 30%, 30vw 100%, 0vw 100%); z-index: 2;">
+        <div class="container">
+          <div class="flex flex-col" style="width:30vw;">
+            <h2 class="text-white w-20">CONTACTS</h2>
+            <div class="flex flex-col">
+              <span class="font-bold text-2xl">Martin Kimla</span>
+              <span class="text-sm text-gray-400 mb-2 -mt-1">ceo & video specialist</span>
+              <a class="" href="mailto:martin.kimla@edya.cz">martin.kimla@edya.cz</a>
+              <a class="" href="tel:+420 732 222 590">+420 732 222 590</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="hidden md:block absolute drop-shadow right-0 bottom-0 w-screen " style="height: 30vw; background: #101010; clip-path: polygon(20vw 30vw, 35vw 24.48vw, 35vw 30vw); z-index: 3;"></div>
+
+      <div class="relative" style="background-color: #FFB801; clip-path: polygon(0 21vw, 100% 0, 100% 100%, 0 100%); min-height: 21vw">
+        <div class="hidden md:block absolute right-0 bottom-0 w-screen" style="height: 30vw; z-index: -1; filter: drop-shadow(0 4px 22px #101010);">
+          <div class="w-screen" style="height: 30vw; background: #101010; clip-path: polygon(20vw 30vw, 100vw 0, 100vw 15vw, 60vw 30vw); "></div>
+        </div>
+        <div class="container stripes-reversed pt-12 md:pt-48 xl:pt-38 pb-36">
+            <div class=""></div>
+            <div class="flex items-center justify-center">
+              <div class="w-4/5">
+                <h2 class="text-black">SEND US A MESSAGE</h2>
+                <form v-on:submit.prevent="submitForm()">
+                  <div class="grid md:grid-cols-2 gap-4 md:gap-8">
+                    <input class="input" type="text" name="name" v-model="form.name" id="" placeholder="NAME" required>
+                    <input class="input" type="text" name="email" v-model="form.email" id="" placeholder="EMAIL" required>
+                  </div>
+                  <textarea class="textarea" name="" id="message" v-model="form.message" cols="30" rows="10" placeholder="MESSAGE" required></textarea>
+                  <div class="flex justify-center"><input type="submit" class="button submit cursor-pointer" value="SUBMIT"></div>
+                </form>
+                <p v-if="form.status!=''" class="text-black mt-4">{{form.status}}</p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+      
+
+</div>
+</template>
+
+<script>
+import landingSection from "./components/landing.vue"
+
+import jQuery from "jquery";
+const $ = jQuery;
+window.$ = $;
+
+import axios from "axios";
+
+export default {
+  name: 'App',
+  components: {
+    landingSection,
+  },
+
+  data(){
+    return{
+        navOpen: false,
+        form: {
+          name: '',
+          email: '',
+          message: '',
+          status: '',
+        }
+    }
+  },
+
+  methods: {
+    openLink(e){
+      setTimeout(()=>{
+          $([document.documentElement, document.body]).animate({
+              scrollTop: $(e).offset().top
+          }, 500);
+      },750);
+    },
+    submitForm(){
+      var data = new FormData();
+      data.append('name', this.form.name);
+      data.append('email', this.form.email);
+      data.append('message', this.form.message);
+
+      axios.post('/form-submit.php', data)
+        .then(() => {
+          this.form.name = this.form.email = this.form.message = '';
+          this.form.status = 'Děkujeme. Zpráva byla úspěšně odeslána!'
+        })
+        .catch((error) => {
+          this.form.status = 'Nastala chyba! Kontaktujte nás prosím telefonicky. ' + error.response.status;
+        })
+    },
+  },
+}
+</script>
+
+<style>
+.service-code{
+  background-image: url('@/assets/code.png');
+}
+.service-photo{
+  background-image: url('@/assets/photo.png');
+}
+.service-video{
+  background-image: url('@/assets/video.png');
+}
+</style>
---
+++ src/components/landing.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="landing" class="w-screen min-h-screen pb-8 relative">
+        <div class="container">
+            <div class="grid grid-cols-2">
+                <div class="w-24 box-border py-8 lg:w-32 lg:py-0">
+                    <svg id="a3f116a1-f5f8-4354-ba7b-710dc320000e" data-name="Vrstva 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 154 78.1"><path d="M85.9,66.6H55.2V11.3H67.6L85.9,45.7Z" transform="translate(0 0)" style="fill:#f6ab20"/><path d="M74.5,0h2.8l23,45.7V78.1H97.8V45.7" transform="translate(0 0)" style="fill:#f6ab20"/><path d="M89.9,0h32.3L106,34.6" transform="translate(0 0)" style="fill:#f6ab20"/><path d="M134.5,0h2.9L114.2,45.7V78.1h-2.5V45.7" transform="translate(0 0)" style="fill:#f6ab20"/><path d="M125.6,49.9V45.7l17.1-34.4H154V49.9Z" transform="translate(0 0)" style="fill:#f6ab20"/><path d="M154,60.7H125.6V78H154Z" transform="translate(0 0)" style="fill:#f6ab20"/><path d="M0,2V76.1H8.1V2" transform="translate(0 0)" style="fill:#cfd8d7"/><path d="M46.8,2V9.1h-33V2Z" transform="translate(0 0)" style="fill:#cfd8d7"/><path d="M33.8,36.1h-20v5.6H33.9V36.1Z" transform="translate(0 0)" style="fill:#cfd8d7"/><path d="M13.8,69v7.1H46.9V69Z" transform="translate(0 0)" style="fill:#cfd8d7"/></svg>
+                </div>
+
+            </div>
+            <div style="margin-top: 13vh">
+                <h1>WE ARE<br>EDYA</h1>
+                <div class="subtitle">young, dynamically <br class="lg:hidden">growing <br class="hidden lg:block">company creating <br class="lg:hidden">visual<br class="hidden lg:block"> ideas, either digital<br class=""> or physical</div>
+            </div>
+        </div>
+        <div class="hidden lg:block absolute bottom-0 side-finger" style="transform: translateY(5vh);">
+            <svg height="auto" viewBox="0 0 472 353" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M-109.845 142.673L471.155 352.673L471.155 262.173L-109.845 54.989L-109.845 142.673Z" fill="url(#paint0_linear_210_11)"/>
+                <path d="M-151.401 104.249L377.155 292.809L377.155 194.294L-151.401 0.132221L-151.401 104.249Z" fill="url(#paint1_linear_210_11)"/>
+                <defs>
+                <linearGradient id="paint0_linear_210_11" x1="-113.543" y1="248.273" x2="429.95" y2="105.412" gradientUnits="userSpaceOnUse">
+                <stop offset="0.5" stop-color="#EEB13A"/>
+                <stop offset="1" stop-color="#EEBB3A" stop-opacity="0"/>
+                </linearGradient>
+                <linearGradient id="paint1_linear_210_11" x1="-113.543" y1="248.273" x2="429.95" y2="105.412" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FFB800"/>
+                <stop offset="0.541667" stop-color="#FFB800"/>
+                <stop offset="1" stop-color="#DBA10B" stop-opacity="0.22"/>
+                </linearGradient>
+                </defs>
+            </svg>
+        </div>
+
+        <div class="absolute left-0 bottom-0 h-1/3 w-screen" style="background: linear-gradient(0deg, rgba(0,0,0,0), #202020); transform: translateY(30vh); z-index: -5;"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'landingSection'
+}
+</script>
+
+<style>
+    #landing{
+        background-image: linear-gradient(0deg, #202020, #202020 3%,rgba(0,0,0,0) 30%, rgba(0,0,0,0)) ,url('@/assets/landing.jpg');
+        /* background-attachment: fixed; */
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: 35% 50%;
+    }
+</style>
